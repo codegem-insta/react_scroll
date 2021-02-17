8bdf662243f1653c1b8ed2a56a1add5aeb008c8a
Codegem <codegemorg@gmail.com>
2021-02-16 23:17:20 -0500

    Aligned and sized posts

--- src/components/UserPosts/UserPosts.js
+++ src/components/UserPosts/UserPosts.js
@@ -3,23 +3,24 @@ import style from './UserPosts.module.css';
 
 function allPosts() {
     return [
-        { id: 0, name: "Michael Scott", content: "\"You miss 100% of the\n shots you don't take – Wayne Gretzky\" – Michael Scott" },
-        { id: 1, name: "Neil deGrasse Tyson", content: "We are part of this universe; we are in this universe, but perhaps more important than both of those facts, is that the universe is in us." },
+        { id: 0, name: "Michael Scott", content: "\"You miss 100% of the shots you\n don't take – Wayne Gretzky\"" },
+        { id: 1, name: "Neil deGrasse Tyson", content: "If each dead person became a ghost,\n there’d be more than 100-billion of\nthem haunting us all. Creepy, but cool." },
         { id: 2, name: "Walter White", content: "I am the one who knocks" },
     ];
 }
 
 function Post(props) {
     const post = props.post;
+    const content = post.content.split('\n').map(line => <p>{line}</p>) 
 
     return (
-        <div>
-            <div>
-                <div></div>
+        <div className={style.Post}>
+            <div className={style.Header}>
+                <div className={style.Picture}></div>
                 {post.name}
             </div>
-            <div>
-                {post.content}
+            <div className={style.Content}>
+                {content}
             </div>
         </div>
     );
