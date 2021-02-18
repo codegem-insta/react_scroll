86dbb299cc628cba0b140fd4bfe548e69e6fd118
Codegem <codegemorg@gmail.com>
2021-02-18 14:12:27 -0500

    Used prettier to format code

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,46 +1,45 @@
-import { useEffect, useState } from 'react';
-import style from './ProgressBar.module.css';
-
-
+import { useEffect, useState } from "react";
+import style from "./ProgressBar.module.css";
 
 function calculateScrollPercentage() {
-    const scrollOffset = window.pageYOffset;
-    const scrollPercentage = Math.floor(scrollOffset / totalPageHeight() * 100);
+  const scrollOffset = window.pageYOffset;
+  const scrollPercentage = Math.floor((scrollOffset / totalPageHeight()) * 100);
 
-    return scrollPercentage;
+  return scrollPercentage;
 }
 
 function totalPageHeight() {
-    const windowHeight = window.innerHeight;
-    const docHeight = document.documentElement.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const docHeight = document.documentElement.scrollHeight;
 
-    return docHeight - windowHeight;
+  return docHeight - windowHeight;
 }
 
 export function useScroll() {
-    const [percentageScrolled, setPercentageScrolled] = useState(0);
+  const [percentageScrolled, setPercentageScrolled] = useState(0);
 
-    useEffect(() => {
-        const scroll = () => setPercentageScrolled(calculateScrollPercentage());
+  useEffect(() => {
+    const scroll = () => setPercentageScrolled(calculateScrollPercentage());
 
-        document.addEventListener('scroll', scroll);
+    document.addEventListener("scroll", scroll);
 
-        return () => document.removeEventListener('scroll', scroll);
-    }, []);
+    return () => document.removeEventListener("scroll", scroll);
+  }, []);
 
-    return percentageScrolled;
+  return percentageScrolled;
 }
 
-
 function ProgressBar() {
-    const startX = 20;
-    const scaleDown = (100-startX)/100;
-    const percentScroll = startX + useScroll(startX)*scaleDown;
+  const startX = 20;
+  const scaleDown = (100 - startX) / 100;
+  const percentScroll = startX + useScroll(startX) * scaleDown;
 
-    return (
-        <div className={style.ProgressBar} style={{ width: percentScroll + '%' }}>
-        </div>
-    )
+  return (
+    <div
+      className={style.ProgressBar}
+      style={{ width: percentScroll + "%" }}
+    ></div>
+  );
 }
 
 export default ProgressBar;
